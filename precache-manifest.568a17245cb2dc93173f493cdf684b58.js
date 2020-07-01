79c7fdfc849678d8cae599feb38dee7854b2b2a1
Sukanya1998 <sgvd.off@gmail.com>
2020-07-01 19:34:36 +0530

    Updates

---
+++ precache-manifest.568a17245cb2dc93173f493cdf684b58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cef8d9f258faf19e8d5443acfc7486d",
+    "url": "/Anuska-Birthday/index.html"
+  },
+  {
+    "revision": "371ffc9299a420fd9a36",
+    "url": "/Anuska-Birthday/static/css/main.7f5bf2ec.chunk.css"
+  },
+  {
+    "revision": "c958ae7f397646db5f94",
+    "url": "/Anuska-Birthday/static/js/2.bbe99f15.chunk.js"
+  },
+  {
+    "revision": "4ecd42502bdf997fb2c4f5b57652fefa",
+    "url": "/Anuska-Birthday/static/js/2.bbe99f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371ffc9299a420fd9a36",
+    "url": "/Anuska-Birthday/static/js/main.e7591dd0.chunk.js"
+  },
+  {
+    "revision": "48c3204468ec0bfc0ff4",
+    "url": "/Anuska-Birthday/static/js/runtime-main.54cbc7b4.js"
+  },
+  {
+    "revision": "e2cb2a04fcc3c041395957dd4e3b7ace",
+    "url": "/Anuska-Birthday/static/media/bg2.e2cb2a04.jpg"
+  },
+  {
+    "revision": "759e5c9425884ad1fab73ee060809132",
+    "url": "/Anuska-Birthday/static/media/bg3.759e5c94.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Anuska-Birthday/static/media/logo.ee7cd8ed.svg"
+  }
+]);
